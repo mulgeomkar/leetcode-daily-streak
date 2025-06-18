58b591c4fca412c24f5530c420a4569291f1d361
MULGE OMKAR <115933816+mulgeomkar@users.noreply.github.com>
2025-06-19 01:24:11 +0530

    Update Solution2966.java

--- Solution2966.java
+++ Solution2966.java
@@ -1,12 +1,12 @@
-class Solution2966 {
-    public List<List<Integer>> divideArray(int[] nums, int k) {
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
         int n = nums.length;
         
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 2; i < n; i += 3) {
             if (nums[i] - nums[i - 2] > k) {
-                return new ArrayList<>();
+                return new int[0][];
             }
             List<Integer> triplet = new ArrayList<>();
             triplet.add(nums[i - 2]);
@@ -14,6 +14,13 @@ class Solution2966 {
             triplet.add(nums[i]);
             res.add(triplet);
         }
-        return res;
+        
+        int[][] ans = new int[res.size()][3];
+        for (int i = 0; i < res.size(); i++) {
+            for (int j = 0; j < 3; j++) {
+                ans[i][j] = res.get(i).get(j);
+            }
+        }
+        return ans;
     }
 }
