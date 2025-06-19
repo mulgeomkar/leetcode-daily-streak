65300ed4368ad8460a844eaa92adc1bb62264c88
MULGE OMKAR <115933816+mulgeomkar@users.noreply.github.com>
2025-06-19 23:32:12 +0530

    Update Solution2294.java

--- Solution2294.java
+++ Solution2294.java
@@ -2,11 +2,11 @@ class Solution2294{
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
         int ans = 1;
-        int rec = nums[0];
+        int count = nums[0];
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] - rec > k) {
+            if (nums[i] - count > k) {
                 ans++;
-                rec = nums[i];
+                count = nums[i];
             }
         }
         return ans;
